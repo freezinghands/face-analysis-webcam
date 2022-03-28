0cebc64d839b5577f657467792637f8eae81701b
freezinghands <jy021316@gmail.com>
2022-03-28 11:22:59 +0900

    Updated additional packages needed

--- setup-required-packages.sh
+++ setup-required-packages.sh
@@ -2,3 +2,5 @@
 python3 -m pip install datetime imutils opencv-python requests Pillow
 python3 -m pip install --upgrade azure-cognitiveservices-vision-face
 #python3 -m pip install google-cloud-vision
+python3 -m pip install awscli
+python3 -m pip install boto3
