d1204015eea7d5eb5c200b6c5d2aa2bd8ff77ef5
freezinghands <jy021316@gmail.com>
2022-03-31 23:28:11 +0900

    Added feature (uploads file to S3 bucket)

--- sideProj/main-awss3-imageupload.py
+++ sideProj/main-awss3-imageupload.py
@@ -28,17 +28,54 @@ def create_presigned_url(bucket_name, object_name, expiration=3600):
     return response
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 #url = create_presigned_url('BUCKET_NAME', 'OBJECT_NAME')
 bucket_name = 'jongsul'
-filename = 'face-20220313-000128.jpg'
+filename = 'samplepng.jpg'
 
 
 if __name__ == '__main__':
-    # setup(settings)
+    import time
+    # current working directory
     print(os.path.dirname(os.path.abspath(__file__)))
-    os.environ['AWS_SHARED_CREDENTIALS_FILE'] = os.path.dirname(os.path.abspath(__file__)) + "\\awsconfig.ini"
-    
-    # execute
-    url = create_presigned_url(bucket_name, filename)
-    print(f"filename: {filename}")
-    print(f"Your url: {url}")
+    print()
+    # env variable setting
+    os.environ['AWS_SHARED_CREDENTIALS_FILE'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "awsconfig.ini")
+
+    s0 = time.time()
+    # upload local file to S3 bucket
+    if upload_file(filename, bucket_name):
+        print("upload success.")
+        s1 = time.time()
+        # create url
+        url = create_presigned_url(bucket_name, filename)
+        print(f"filename: {filename}")
+        print(f"Your url: {url}")
+    else:
+        print("upload denied.")
+    s2 = time.time()
+    print(f'total time: {s2 - s0}')
+    print(f'upload time: {s1 - s0}')
+    print(f'url creation time: {s2 - s1}')
